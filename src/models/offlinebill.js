2a48d826fea1eb55e774aac5f5cb13d062175301
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-04 00:15:10 +0530

    add offline bill data model

---
+++ src/models/offlinebill.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const offlineBillSchema = new mongoose.Schema({
+    dishes: [{
+        dishName:{
+            type: String,
+            required: true,
+            trim: true
+        },
+        price:{
+            type: Number,
+            required: true
+        },
+        quantity:{
+            type: Number, 
+            required: true
+        },
+        itemTotal: {
+            type: Number,
+            required: true
+        }
+    }],
+    subtotal: {
+        type: Number,
+        required: true
+    },
+    tax:{
+        type: Number,
+        required: true
+    },
+    grandTotal:{
+        type: Number,
+        required: true
+    }
+},
+{
+    timestamps: true
+}
+)
+
+export const OfflineBill = mongoose.model('offlineBill', offlineBillSchema)
