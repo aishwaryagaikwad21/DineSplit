2605fb80d581661d0fb2f96ca02558351daccc43
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-11 16:09:24 +0530

    chore: set up React frontend with Tailwind and shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <>
+      <div className="text-4xl font-bold">App</div>
+      <Button variant="outline">Button</Button>
+    </>
+  )
+}
+
+export default App
