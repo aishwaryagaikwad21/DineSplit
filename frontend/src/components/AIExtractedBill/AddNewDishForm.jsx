3c43727d026d5f3c2ceb93df6f5729fec53d58cd
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-24 20:35:40 +0530

    feat: Add New Dish Form

--- frontend/src/components/AIExtractedBill/AddNewDishForm.jsx
+++ frontend/src/components/AIExtractedBill/AddNewDishForm.jsx
@@ -1,8 +1,91 @@
 import React from 'react'
+import { X } from "lucide-react"
+import { Button } from '@base-ui/react/button'
 
-const AddNewDishForm = () => {
+const AddNewDishForm = ({
+    newDish,
+    onChange,
+    onSubmit,
+    onCancel
+}) => {
   return (
-    <div>AddNewDishForm</div>
+        <div className="mx-auto my-5 max-w-4xl rounded-xl border p-5">
+
+                    <h2 className="flex justify-between mb-5 text-xl font-semibold">
+                        Add Dish 
+                        <X className='cursor-pointer' onClick={onCancel} />
+                    </h2>
+
+                    <div className="grid gap-4 md:grid-cols-3">
+
+                        {/* Dish Name */}
+                        <div className="flex flex-col gap-1">
+                            <label>Dish Name</label>
+
+                            <input
+                                type="text"
+                                value={newDish.dishName}
+                                onChange={(e) =>
+                                    onChange(
+                                        "dishName",
+                                        e.target.value
+                                    )
+                                }
+                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
+                                required
+                            />
+                        </div>
+
+                        {/* Price */}
+                        <div className="flex flex-col gap-1">
+                            <label>Price</label>
+
+                            <input
+                                type="number"
+                                value={newDish.price}
+                                onChange={(e) =>
+                                    onChange(
+                                        "price",
+                                        e.target.value
+                                    )
+                                }
+                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
+                                required
+                            />
+                        </div>
+
+                        {/* Quantity */}
+                        <div className="flex flex-col gap-1">
+                            <label>Quantity</label>
+
+                            <input
+                                type="number"
+                                min="1"
+                                value={newDish.quantity}
+                                onChange={(e) =>
+                                    onChange(
+                                        "quantity",
+                                        e.target.value
+                                    )
+                                }
+                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
+                                required
+                            />
+                        </div>
+
+                    </div>
+
+                <div className="mt-5 flex justify-center">
+                    <Button
+                        type="button"
+                        onClick={onSubmit}
+                        className="cursor-pointer rounded-xl bg-amber-400 px-6 py-2 font-semibold text-zinc-900 hover:bg-amber-500"
+                    >
+                        Submit
+                    </Button>
+                </div>
+
+            </div>
   )
 }
 
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import billData from '../sampleData-1.json'
-import { Button } from '@base-ui/react/button'
 import { X } from "lucide-react"
+import { Button } from '@base-ui/react/button'
+
 import BillDishes from '@/components/AIExtractedBill/BillDishes'
+import AddNewDishForm from '@/components/AIExtractedBill/AddNewDishForm'
 
 const TestPage = () => {
 
@@ -267,84 +269,12 @@ const TestPage = () => {
             />
 
             {addFormTogg && (
-                <div className="mx-auto my-5 max-w-4xl rounded-xl border p-5">
-                    
-
-                    <h2 className="flex justify-between mb-5 text-xl font-semibold">
-                        Add Dish 
-                        <X className='cursor-pointer' onClick={() => setAddFormTogg((prevTogg) => !prevTogg) } />
-                    </h2>
-
-                    <div className="grid gap-4 md:grid-cols-3">
-
-                        {/* Dish Name */}
-                        <div className="flex flex-col gap-1">
-                            <label>Dish Name</label>
-
-                            <input
-                                type="text"
-                                value={newDish.dishName}
-                                onChange={(e) =>
-                                    handleNewDishChange(
-                                        "dishName",
-                                        e.target.value
-                                    )
-                                }
-                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
-                                required
-                            />
-                        </div>
-
-                        {/* Price */}
-                        <div className="flex flex-col gap-1">
-                            <label>Price</label>
-
-                            <input
-                                type="number"
-                                value={newDish.price}
-                                onChange={(e) =>
-                                    handleNewDishChange(
-                                        "price",
-                                        e.target.value
-                                    )
-                                }
-                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
-                                required
-                            />
-                        </div>
-
-                        {/* Quantity */}
-                        <div className="flex flex-col gap-1">
-                            <label>Quantity</label>
-
-                            <input
-                                type="number"
-                                min="1"
-                                value={newDish.quantity}
-                                onChange={(e) =>
-                                    handleNewDishChange(
-                                        "quantity",
-                                        e.target.value
-                                    )
-                                }
-                                className="rounded-lg border px-3 py-2 outline-none focus:border-amber-400"
-                                required
-                            />
-                        </div>
-
-                    </div>
-
-                <div className="mt-5 flex justify-center">
-                    <Button
-                        type="button"
-                        onClick={handleAddDish}
-                        className="cursor-pointer rounded-xl bg-amber-400 px-6 py-2 font-semibold text-zinc-900 hover:bg-amber-500"
-                    >
-                        Submit
-                    </Button>
-                </div>
-
-            </div>
+                <AddNewDishForm
+                    newDish = {newDish} 
+                    onChange = {handleNewDishChange}
+                    onSubmit = {handleAddDish}
+                    onCancel = {() => setAddFormTogg((prevTogg) => !prevTogg) }
+                />
         )}
 
             <div className='flex justify-center my-2'>
